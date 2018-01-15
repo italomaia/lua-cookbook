9d867976599147f0ea68c5eaa9cdca344b1aa464
Italo Maia <italo.maia@gmail.com>
2018-01-15 20:21:59 -0300

    Problem 01.06

--- 01.06/main.lua
+++ 01.06/main.lua
@@ -2,3 +2,9 @@
 Problem: You have several small strings that you need to combine into
 a larger string
 ]]
+
+require 'busted.runner'()
+
+it('combines strings', function ()
+  assert.are.equal(table.concat({'hello', ' ', 'world'}), 'hello world')
+end)
