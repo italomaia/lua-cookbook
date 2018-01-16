b52733240f02a31c7fdb3b0f7674a55601ee8631
Italo Maia <italo.maia@gmail.com>
2018-01-15 22:53:50 -0300

    Problem 01.08

--- 01.08/main.lua
+++ 01.08/main.lua
@@ -2,3 +2,17 @@
 Problem: You need to check for the occurrence of any of a set of characters
 in a string
 ]]
+
+require 'busted.runner'()
+
+function has_some(chars, text)
+  for c in chars:gmatch("%a") do
+    if text:find(c) then return true end
+  end
+  return false
+end
+
+it('text has some char', function()
+  assert.is_false(has_some('abc', 'hello world'))
+  assert.is_true(has_some('def', 'hello world'))
+end)
