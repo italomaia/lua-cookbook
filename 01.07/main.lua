b6c6a73d5c2f91b9187612ee8d4ff134d3c704bf
Italo Maia <italo.maia@gmail.com>
2018-01-15 22:11:28 -0300

    Problem 01.07

--- 01.07/main.lua
+++ 01.07/main.lua
@@ -1,1 +1,20 @@
 -- Problem: you want to reverse the characters or words in a string.
+
+require 'busted.runner'()
+
+function reverse (word) return word:reverse() end
+function reverse_phrase (phrase)
+  local words = {}
+  for word in phrase:gmatch('[^ ]+') do
+    table.insert(words, 1, word)
+  end
+  return table.concat(words, ' ')
+end
+
+it('word can be reversed', function ()
+  assert.are.equal(reverse('hello'), 'olleh')
+end)
+
+it('phrase can be reversed', function ()
+  assert.are.equal(reverse_phrase('hello world'), 'world hello')
+end)
