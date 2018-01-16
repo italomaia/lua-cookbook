10dc5bff11ec92e160d0430665e87da78f699990
Italo Maia <italo.maia@gmail.com>
2018-01-15 22:58:00 -0300

    Problem 01.10 (no solution yet)

---
+++ 01.10/main.lua
@@ -0,0 +1,5 @@
+--[[
+Problem: Given a set of characters to keep, you need to build a filtering
+function that, applied to any string s, returns a copy of s that contains only
+characters in the set.
+]]
